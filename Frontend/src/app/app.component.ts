ee0499b65d91276841f16c0fab1f62c8acdae905
MarianoJuly <marcianocruzeiri@gmail.com>
2023-11-25 20:10:33 -0300

    estrutura front

---
+++ Frontend/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HeadComponent } from "./components/head/head.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, HeadComponent]
+})
+export class AppComponent {
+  title = 'Frontend';
+}
