4afdbc08d7a55a969c88a2b417296ed1f5d53336
MarianoJuly <marcianocruzeiri@gmail.com>
2023-12-12 11:39:10 -0300

    quase fim de telas

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { RegistrarFuncionariosComponent } from './components/formularios/registrar-funcionarios/registrar-funcionarios.component';
 import { LoginComponent } from './components/formularios/login/login.component';
 import { HotelModalComponent } from './components/hotel-modal/hotel-modal.component';
+import { RegistrarHotelComponent } from './components/formularios/registrar-hotel/registrar-hotel.component';
 
 const routes: Routes = [
   {path: 'modalDatas', component: ModalComponent},
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path: 'home',component: HomeComponent},
   {path: 'login', component: LoginComponent },
   {path: 'listaHotel', component: HotelModalComponent },
+  {path: 'registrarHotel', component: RegistrarHotelComponent}
 ];
 
 @NgModule({
